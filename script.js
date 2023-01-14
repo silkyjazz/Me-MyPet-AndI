6dce3f947b4b465bd55b6e683e45510e10fde9d6
Emil1577 <emilronquillo@gmail.com>
2023-01-13 20:21:46 -0800

    created a favorite function js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 var search = document.querySelector('#search')
-var breeds = "australian"
+var breeds = "chow"
 var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
+var favorites = document.querySelector('#showFavoriteList')
 
 
 search.addEventListener("click", function (event) {
 	event.preventDefault();
-
 
 	fetch(api)
 		.then(function (response) {
@@ -14,14 +14,11 @@ search.addEventListener("click", function (event) {
 			return response.json()
 		})
 		.then(function (data) {
-			console.log("data", data)
-			console.log(data);
+
 			var dogImages = data.message;
 
-			//$('body').append(' <button class=""  id="favorite">Add to favorites</button>');
 			$('#images').attr('src', dogImages);
 			$("#favorite").css("visibility", "visible");
-
 
 			var imageUrl;
 
@@ -32,24 +29,33 @@ search.addEventListener("click", function (event) {
 				var imageUrl = dogImages
 				var savedDogs = JSON.parse(localStorage.getItem('favDogs')) || []
 
+
 				if (!savedDogs.includes(imageUrl)) {
 
 					savedDogs.push(imageUrl);
-			
 				}
-
 				localStorage.setItem('favDogs', JSON.stringify(savedDogs));
-			
-			
-
-
-					console.log(imageUrl);
-
-
 
 			})
-
 		})
-
 })
 
+favorites.addEventListener("click", function (event) {
+	event.preventDefault();
+
+
+
+	var savedDogs = JSON.parse(localStorage.getItem('favDogs')) || []
+
+	for (var i = 0; i < savedDogs.length; i++) {
+		
+console.log(i);
+		var a =i+1
+
+		$('body').append('<img class= id=favorites src='+savedDogs[i]+ '></img>')
+
+	}
+
+	console.log(savedDogs);
+
+});
