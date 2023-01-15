94889a09182a4edf40a18c7adc2b0542175a39f6
Darren Watanabee <watanabedarren@yahoo.com>
2023-01-15 10:42:03 -0800

    created name generator using api

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-
+let listOfAllBreed=[];
 var search = document.querySelector('#search')
-var breeds = "chow"
+var breeds = "chow" // do we need this?
 var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
 var favorites = document.querySelector('#showFavoriteList')
 var clearFavorites = document.querySelector('#clearFavList')
 var welcome = document.getElementById("welcome")
+// create the name generator
+const nameGenerator_box = $(".nameGenerator-Box")[0]
+const nameGenerator = ()=>{
+	const baseUrl = `https://api.fungenerators.com`; // using the name generator api
+	const url = `${baseUrl}/name/generate?category=dog&limit=10`; // generating a list of 10 random names
+	petsName.innerHTML=""
+	fetch(url).then((res)=>{
+		return res.json();
+	}).then((data)=>{
+		console.log(data.contents.names);
+		let {names} = data.contents;
+		for(let i=0;i<names.length;i++){ 
+			const html = ` 
+			<div class='list-item'>
+				<li>
+					<b>${i+1}.</b> ${names[i]} 
+				</li>
+		    </div>`
+			petsName.insertAdjacentHTML('beforeend', html); // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+		}
+	})
 
 search.addEventListener("click", function (event) {
 	event.preventDefault();
@@ -59,3 +80,4 @@ search.addEventListener("click", function (event) {
 
 		}
 	})
+}
