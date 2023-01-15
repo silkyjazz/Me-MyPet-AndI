fec0cbed7e7e37e3d6aac7d307e1956528df1e2b
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-15 15:45:51 -0800

    added more dropdown options for user to choose from

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+/*
+
+We need to get breed options to populate in the dropdown
+
+Once the user selects a breed and clicks the button, the dog's image and info
+should populate on the DOM.
+
+The button is clicked and the user sees cards displaying the dog's image and in rows 4 columns.
+In each card there is a heart icon with the option of saving the dog to 'favorites' or local storage
+
+When the user navigates to the favorites link they are shown all of their favorites with the option to unfavorite
+which will delete the item from local storage
+
+If there are no favorites stored in local storage we want to let the user know 'There are no favorites to display'.
+If items exist, we want the dogs displays with cards in rows and 4 columns
+
+*/
 
 var search = document.querySelector('#search')
-var breeds = "chow"
+var dropdown = document.getElementById('dropdown');
+// var selectedBreed = dropdown.value;
+var breeds = dropdown.value;
+
+var testApi = 'https://dog.ceo/api/breeds/list/all'
 var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
+// var favorites = ;
+
+ 
 var favorites = document.querySelector('#showFavoriteList')
 var clearFavorites = document.querySelector('#clearFavList')
 var welcome = document.getElementById("welcome")
@@ -16,11 +40,10 @@ search.addEventListener("click", function (event) {
 		})
 		.then(function (data) {
 
-			var dogImages = data.message;
+            console.log('data', data);
 
 			$('#images').attr('src', dogImages);
 			$("#favorite").css("visibility", "visible");
-
 
 			var imageUrl;
 
@@ -28,7 +51,7 @@ search.addEventListener("click", function (event) {
 
 				event.preventDefault();
 
-				var imageUrl = dogImages
+				var imageUrl = dogImages;
 				var savedDogs = JSON.parse(localStorage.getItem('favDogs')) || []
 				$("#showFavoriteList").css("visibility", "visible");
 
