67924816d7e4da3c9f08334156301fe0d1a8ab69
Darren Watanabee <watanabedarren@yahoo.com>
2023-01-16 20:24:24 -0800

    added event listener for breed search

--- script.js
+++ script.js
@@ -17,103 +17,16 @@ If items exist, we want the dogs displayed in a table
 
 */
 
-
-// var dropdown = document.getElementById('dropdown');
-// var breeds = dropdown.value;
-
-
 let listOfAllBreed=[];
+let api = "https://dog.ceo/api/"
 var search = document.querySelector('#search')
-
-var breeds = "chow" // this needs to be directed to the dropdown of the html
-var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
-var favorites = document.querySelectorAll('#showFavoriteList')
-// var breeds = "chow" // do we need this?
-
-var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
-// var favorites = ;
-
- 
-var favorites = document.querySelector('#showFavoriteList')
-
-var clearFavorites = document.querySelector('#clearFavList')
-var welcome = document.getElementById("welcome")
-// create the name generator
-const nameGenerator_box = $(".nameGenerator-Box")[0]
-const nameGenerator = ()=>{
-	const baseUrl = `https://api.fungenerators.com`; // using the name generator api
-	const url = `${baseUrl}/name/generate?category=dog&limit=10`; // generating a list of 10 random names
-	petsName.innerHTML=""
-	fetch(url).then((res)=>{
-		return res.json();
-	}).then((data)=>{
-		console.log(data.contents.names);
-		let {names} = data.contents;
-		for(let i=0;i<names.length;i++){ 
-			const html = ` 
-			<div class='list-item'>
-				<li>
-					<b>${i+1}.</b> ${names[i]} 
-				</li>
-		    </div>`
-			petsName.insertAdjacentHTML('beforeend', html); // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
-		}
-	})
-}
-
-const nameGeneratorButton = $(".nameGenerator-button")[0];
-nameGeneratorButton.addEventListener('click', (e)=>{
-	nameGenerator()
-
-})
+// var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
 
 search.addEventListener("click", function (event) {
 	event.preventDefault();
-	let breeds = $("#breed-input")[0].value;
+    let breeds = $("#breed-input")[0].value;
 	// when any h1 tag is present then remove it.
 	if($(".breed-heading")[0])
 	$(".breed-heading")[0].remove();
 
-	
-
-	fetch(api)
-		.then(function (response) {
-			console.log("response", response)
-			return response.json()
-		})
-		.then(function (data) {
-
-            console.log('data', data);
-
-			$('#images').attr('src', dogImages);
-			$("#favorite").css("background-color", "rgb(226, 220, 220)");
-			$("#favorite").css("visibility", "visible");
-
-			var imageUrl;
-
-			$('#favorite').on("click", function (event) {
-
-				event.preventDefault();
-
-				var imageUrl = dogImages;
-				var savedDogs = JSON.parse(localStorage.getItem('favDogs')) || []
-				$("#favorite").css("background-color", "red");
-				
-
-
-				if (!savedDogs.includes(imageUrl)) {
-
-					savedDogs.push(imageUrl);
-				}
-				localStorage.setItem('favDogs', JSON.stringify(savedDogs));
-
-			})
-		})
-
-
-
-	})
-
-}
-
-
+})
