f65d2adc8c0b84da0de089014b084d18a1b2a7f5
Darren Watanabee <watanabedarren@yahoo.com>
2023-01-16 21:01:32 -0800

    implemented for loop to retrieve random names through the fungenerator api

--- script.js
+++ script.js
@@ -28,6 +28,23 @@ const nameGenerator_box = $(".nameGenerator-Box")[0]
 const nameGenerator = ()=>{
 	const baseUrl = `https://api.fungenerators.com`; // using the name generator api
 	const url = `${baseUrl}/name/generate?category=dog&limit=10`; // generating a list of 10 random names
+	petsName.innerHTML=""
+	fetch(url).then((res)=>{ // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+		return res.json();
+	}).then((data)=>{
+		console.log(data.contents.names);
+		let {names} = data.contents;
+		for(let i=0;i<names.length;i++){ //for loop to retrieve the random names
+			const html = ` 
+			<div class='list-item'>
+				<li>
+					<b>${i+1}.</b> ${names[i]} 
+				</li>
+		    </div>`
+			petsName.insertAdjacentHTML('beforeend', html); 
+		}
+	})
+	
 
 search.addEventListener("click", function (event) {
 	event.preventDefault();
