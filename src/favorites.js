802ab3458d369064dc35217ad43efaff55079c43
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-16 23:27:59 -0800

    unsaved items still being stored, fixed favorites script to store and delete saved dogs

--- src/favorites.js
+++ src/favorites.js
@@ -1,45 +1,12 @@
-
-// var search = document.querySelector('#search')
-// var breeds = "chow"
-// var api = "https://dog.ceo/api/breed/" + breeds + "/images/random";
-// var showFavorites = document.querySelector('#showFavoriteList')
-// var clearFavorites = document.querySelector('#clearFavorites')
-
-// showFavoriteDogs();
-
-// function showFavoriteDogs() {
-
-// 	var savedDogs = JSON.parse(localStorage.getItem('favDogs')) || []
-
-// 	for (var i = 0; i < savedDogs.length; i++) {
-// 		console.log(i);
-// 		var a = i + 1
-
-// 		$('body').append('<img class= id=favorites src=' + savedDogs[i] + '></img>')
-// 		$("#showFavoriteList").css("visibility", "hidden");
-
-// 	}
-// }
-
-// clearFavorites.addEventListener("click", function (event) {
-// 	event.preventDefault();
-
-// 	console.log('clear');
-
-// 	localStorage.clear();
-// 	document.location.reload(true);
-
-
-// })
 let cards = $(".cards-container")[0]; //getting the card container
-let savedDogs = JSON.parse(localStorage.getItem("favDogs")) || []; //retreving data from local storage
+let savedDogs = JSON.parse(localStorage.getItem("favoriteDogs")) || []; //retreving data from local storage
 
 //checking if the user does not have items stored in local storage
 if (savedDogs.length === 0) {
   cards.innerHTML = `<h1>No pets in your favorites list</h1>`;
 }
 
-//looping through the saved dogs and using string iterpolation 
+//looping through the saved dogs and using string iterpolation
 for (let i = 0; i < savedDogs.length; i++) {
   const testhtml =
     cards.innerHTML +
@@ -65,16 +32,15 @@ if (savedDogs.length > 0) {
     imageNumber = this.classList[classListSize - 2].split("-")[1];
 
     imageUrl = $(`#img-${imageNumber}`)[0].currentSrc;
-    let savedDogs = JSON.parse(localStorage.getItem("favDogs")) || [];
-    
+    let savedDogs = JSON.parse(localStorage.getItem("favoriteDogs")) || [];
 
     let indxOfImg = savedDogs.findIndex((obj) => obj.imageUrl === imageUrl);
     if (indxOfImg > -1) {
       savedDogs.splice(indxOfImg, 1);
     }
-    localStorage.setItem("favDogs", JSON.stringify(savedDogs));
+    localStorage.setItem("favoriteDogs", JSON.stringify(savedDogs));
     // e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
-    
+
     console.log();
     if (e.target.parentNode.classList.contains("card-image")) {
       console.log(e.target.parentNode);
@@ -91,5 +57,3 @@ if (savedDogs.length > 0) {
     console.log(savedDogs);
   });
 }
-
-
