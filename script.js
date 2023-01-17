482c86f1520fabd5d8cf9c76e748be082c34317b
Darren Watanabee <watanabedarren@yahoo.com>
2023-01-16 21:20:26 -0800

    implemented for loop for autocomplete in search bar

--- script.js
+++ script.js
@@ -114,3 +114,26 @@ function autocomplete(inp, arr) {
 		a.setAttribute("class", "autocomplete-items ");
 		/*append the DIV element as a child of the autocomplete container:*/
 		this.parentNode.appendChild(a);
+		for (i = 0; i < arr.length; i++) {
+			/*check if the item starts with the same letters as the text field value:*/
+			if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+			  /*create a DIV element for each matching element:*/
+			  var b = document.createElement("DIV");
+			  b.setAttribute("class", "has-text-black")
+			  /*make the matching letters bold:*/
+			  b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+			  b.innerHTML += arr[i].substr(val.length);
+			  /*insert a input field that will hold the current array item's value:*/
+			  b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+			  /*execute a function when someone clicks on the item value (DIV element):*/
+			  b.addEventListener("click", function(e) {
+				  /*insert the value for the autocomplete text field:*/
+				  inp.value = this.getElementsByTagName("input")[0].value;
+				  /*close the list of autocompleted values,
+				  (or any other open lists of autocompleted values:*/
+				  closeAllLists();
+			  });
+			  a.appendChild(b);
+			}
+		  }
+	  });
